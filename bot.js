b7c19582382e004d036f018d5887b09e072c4c1f
HamoudAlenzi <47364548+HamoudAlenzi@users.noreply.github.com>
2026-07-06 16:03:32 +0300

    Change static file serving directory in bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // ===== DATA STORE =====
 let store = {
